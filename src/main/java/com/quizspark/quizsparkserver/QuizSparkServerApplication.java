54ddb0c60e263d8837b369f835fbcc8c0333b365
Vũ Long <lnvlong.dev@gmail.com>
2025-10-06 16:40:38 +0700

    🎉: Initialize the project

---
+++ src/main/java/com/quizspark/quizsparkserver/QuizSparkServerApplication.java
@@ -0,0 +1,13 @@
+package com.quizspark.quizsparkserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizSparkServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuizSparkServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/quizspark/quizsparkserver/QuizSparkServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quizspark.quizsparkserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizSparkServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
